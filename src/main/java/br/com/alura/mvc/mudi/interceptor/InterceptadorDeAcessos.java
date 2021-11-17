b4df70e405fa1f5324e8947fcdb878a149d08b56
Raul <raultorres.lima@gmail.com>
2021-11-17 15:47:10 -0300

    Add interceptor

--- src/main/java/br/com/alura/mvc/mudi/interceptor/InterceptadorDeAcessos.java
+++ src/main/java/br/com/alura/mvc/mudi/interceptor/InterceptadorDeAcessos.java
@@ -6,23 +6,58 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InterceptadorDeAcessos  extends HandlerInterceptorAdapter {
 
+    public static List<Acesso> acessos = new ArrayList<Acesso>();
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return super.preHandle(request, response, handler);
 
+        Acesso acesso = new Acesso();
+        acesso.path = request.getRequestURI();
+        acesso.data = LocalDateTime.now();
+
+        request.setAttribute("acesso", acesso);
+        return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        super.afterCompletion(request, response, handler, ex);
+        Acesso acesso = (Acesso) request.getAttribute("acesso");
+        acesso.duracao = Duration.between(acesso.data, LocalDateTime.now());
+        acessos.add(acesso);
     }
 
-    class Acesso {
+    public static class Acesso {
         private String path;
         private LocalDateTime data;
         private Duration duracao;
+
+        public String getPath() {
+            return path;
+        }
+
+        public void setPath(String path) {
+            this.path = path;
+        }
+
+        public LocalDateTime getData() {
+            return data;
+        }
+
+        public void setData(LocalDateTime data) {
+            this.data = data;
+        }
+
+        public Duration getDuracao() {
+            return duracao;
+        }
+
+        public void setDuracao(Duration duracao) {
+            this.duracao = duracao;
+        }
     }
 }
